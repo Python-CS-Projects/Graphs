f929b8709232a3adf405a951b98ad6ac8f061e14
fritzgt <tuanis@live.com>
2020-04-13 12:13:10 +0300

    testing implementation

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
+# Note: This Queue class is sub-optimal. Why?
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
 
 def earliest_ancestor(ancestors, starting_node):
 
@@ -20,19 +37,35 @@ def earliest_ancestor(ancestors, starting_node):
     # 2.Create BFS
 
     # Create a queue
+    q = Queue()
     # Initiate a path with the starting_node
+    path = [starting_node]
     # Enqueue path
+    q.enqueue(path)
     # Create a set for visted
+    visited = set()
     # While queue is not empty
+    while q.size() > 0:
         # dequeue current path and save in var
+        current_path = q.dequeue()
+        print(f"Current path {current_path}")
         # *Think* Get last node in path [-1]
+        current_node = current_path[-1]
         # check if current node is not visited
+        if current_node not in visited:
             # add to visited
+            visited.add(current_node)
             # iterate on the current_node neighbors
-                #Create a copy of the path
-                #append neghbors to the path_copy
-                #enqueue path_copy
+            for neighbor in vertices[current_node]:
+                print(f"Neighbor: {neighbor}")
+                # Create a copy of the path
+                path_copy = current_path
+                # append neghbors to the path_copy
+                path_copy.append(neighbor)
+                # enqueue path_copy
+                q.enqueue(path_copy)
 
-    # 3.Initiate Graph with the ancestors list
+    # 3.Compare paths to return the longest
+
     print(f"Vertices {vertices}")
     pass
