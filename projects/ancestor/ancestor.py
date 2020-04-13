6d9e824e7710993827f7b760faf76db75c190adf
fritzgt <tuanis@live.com>
2020-04-13 11:45:09 +0300

    working on earliest ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,25 @@
 
 def earliest_ancestor(ancestors, starting_node):
+
+    # 1.Create helper methods to Build Graph
+
+    # iniciate vertices
+    vertices = {}
+    # Add Nodes
+    for ancestor in ancestors:
+        # print(ancestor[0])
+        # Create vertices
+        vertix_id = ancestor[1]
+        vertices[vertix_id] = set()
+    # Add edges
+    for node in ancestors:
+        vertix_id = node[1]
+        vertix_2 = node[0]
+        vertices[vertix_id].add(vertix_2)
+    # get neighbors
+
+        # 2.Create BFS
+
+        # 3.Initiate Graph with the ancestors list
+    print(f"XXX {vertices}")
     pass
