66f1a0ad4af4f61ad9a79f7f6206c1b3f70be494
fritzgt <tuanis@live.com>
2020-04-21 05:17:40 +0300

    working on DFT

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -6,30 +6,15 @@ import random
 from ast import literal_eval
 
 
-class Queue():
-    def __init__(self):
-        self.queue = []
-
-    def enqueue(self, value):
-        self.queue.append(value)
-
-    def dequeue(self):
-        if self.size() > 0:
-            return self.queue.pop(0)
-        else:
-            return None
-
-    def size(self):
-        return len(self.queue)
-
+from util import Stack, Queue
 
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -55,12 +40,11 @@ Useful commands:
 `player.current_room.get_exits()`
 `player.travel(direction)`
 
-Final:
+Adjancency list:
 {
   0: {'n': '?', 's': '?', 'w': '?', 'e': '?'}
 }
 """
-
 # print(f"Rooms: {room_graph}")
 # print("----------------------")
 # print(f"Current room: {player.current_room.id}")
@@ -71,24 +55,37 @@ Final:
 # print(f"Current room: {player.current_room.id}")
 # print(f"Exits: {player.current_room.get_exits()}")
 # print("----------------------")
+print("----------------------")
+print("Start of Solution.")
+print("----------------------")
+
+# Graph
+graph = {}
+# Add "?" to the exits
 
 
-# Diccionary of rooms explored
-
-# List of the path to get back
-
-# List of opposite directions
-
-# Get current room
-
-# add current room to the explored dic
-
-# iterate over the neighbors of the current room
+def pre_graph():
+    # Initialize the nested diccionary
+    graph[player.current_room.id] = {}
+    for x in player.current_room.get_exits():
+        # Add "?" to each exit
+        graph[player.current_room.id][x] = "?"
 
 
-"""
-End of Solution.
-"""
+pre_graph()
+print(graph)
+# Create a DFT
+
+# BFS to search for rooms with a question mark
+
+# BFS will return a list of id's we need to convert to coordinates
+
+# if all paths has been explored then return the list append to traversal_path
+
+
+print("----------------------")
+print("End of Solution.")
+print("----------------------")
 
 # TRAVERSAL TEST
 visited_rooms = set()
